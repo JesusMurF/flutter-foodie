a8df3bfe29db77df84a975b4450738c120c30b97
Jesus Mur Fontanals <murfontanals@gmail.com>
2020-03-18 21:37:42 +0100

    :tada: first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(new MyApp());
+}
+
+// Aplicacion basica
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+        home: Text('Hello world',
+            style: TextStyle(color: Colors.white, fontSize: 55.0)));
+  }
+}
