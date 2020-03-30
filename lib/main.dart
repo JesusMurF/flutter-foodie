c998a381959e6f398bc6c2a355395b34d05a4542
Jesús Mur Fontanals <murfontanals@gmail.com>
2020-03-30 20:24:11 +0200

    :lipstick: final distribution

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
                 image: AssetImage("images/Home.png"), fit: BoxFit.cover),
           ),
           child: Container(
-              margin: EdgeInsets.only(top: 100.0), child: _foodieContainer()),
+              margin: EdgeInsets.only(top: 150.0), child: _foodieContainer()),
         ),
       ),
     );
@@ -30,12 +30,9 @@ class MyApp extends StatelessWidget {
 
   Widget _foodieColumn() {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         mainAxisSize: MainAxisSize.max,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+          Column(mainAxisAlignment: MainAxisAlignment.start, children: [
             Image.asset('images/rosco.png'),
             SizedBox(height: 20),
             Text(
@@ -51,16 +48,30 @@ class MyApp extends StatelessWidget {
                     fontSize: 20.0,
                     fontWeight: FontWeight.w400)),
           ]),
+          Expanded(child: _orderButtonColumn())
+        ]);
+  }
+
+  Widget _orderButtonColumn() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 80.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        // mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
           RaisedButton(
-            elevation: 6.0,
+              elevation: 6.0,
               shape: RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0),),
+                borderRadius: new BorderRadius.circular(10.0),
+              ),
               onPressed: () {},
               child: Text('Start Ordering'),
               color: Colors.white,
               textColor: Colors.black45,
               padding: const EdgeInsets.only(
-                  left: 32.0, right: 32.0, top: 16.0, bottom: 16.0))
-        ]);
+                  left: 32.0, right: 32.0, top: 16.0, bottom: 16.0)),
+        ],
+      ),
+    );
   }
 }
