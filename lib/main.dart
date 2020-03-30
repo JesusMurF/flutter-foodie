b8012034b6375afcaa579dc02d7c889317f715ab
Jesús Mur Fontanals <murfontanals@gmail.com>
2020-03-30 20:13:39 +0200

    :sparkles: added distribution design

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,54 @@ class MyApp extends StatelessWidget {
             image: DecorationImage(
                 image: AssetImage("images/Home.png"), fit: BoxFit.cover),
           ),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [Image.asset('images/rosco.png')])
-            ],
-          ),
+          child: Container(
+              margin: EdgeInsets.only(top: 100.0), child: _foodieContainer()),
         ),
       ),
     );
+  }
+
+  Widget _foodieContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[_foodieColumn()],
+    );
+  }
+
+  Widget _foodieColumn() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+            Image.asset('images/rosco.png'),
+            SizedBox(height: 20),
+            Text(
+              'Foodie',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text('one-click food',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w400)),
+          ]),
+          RaisedButton(
+            elevation: 6.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10.0),),
+              onPressed: () {},
+              child: Text('Start Ordering'),
+              color: Colors.white,
+              textColor: Colors.black45,
+              padding: const EdgeInsets.only(
+                  left: 32.0, right: 32.0, top: 16.0, bottom: 16.0))
+        ]);
   }
 }
