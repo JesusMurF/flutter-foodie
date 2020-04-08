6728c7f9caf38bbfbfb2754edf9c08fa34228b73
Jesús Mur Fontanals <murfontanals@gmail.com>
2020-04-08 21:38:23 +0200

    :sparkles: appBar custom started

--- lib/pages/home_list.dart
+++ lib/pages/home_list.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
 class HomeList extends StatelessWidget {
+  final Color _appBarColorText = Color.fromRGBO(200, 44, 100, 1);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home List'),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100.0),
+          child: AppBar(
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                icon: Icon(Icons.menu, color: _appBarColorText, size: 20.0),
+                onPressed: () {},
+              ),
+              title: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(right: 12.0),
+                    child: Text(
+                      'Deliver to:',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14.0),
+                    ),
+                  ),
+                  Text(
+                    'Hobskin Road, CA',
+                    style: TextStyle(
+                        color: _appBarColorText,
+                        fontSize: 16.0,
+                        letterSpacing: -1),
+                  )
+                ],
+              )),
         ),
         body: Center(
           child: Container(
